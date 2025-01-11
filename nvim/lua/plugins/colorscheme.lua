9f2427c39c0ae25af213f1b588d4a7afeece75af
wesjos <142285064+nonwesjoe@users.noreply.github.com>
2025-01-11 21:19:54 +0800

    Add files via upload

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    style = "moon", -- 可选值：storm, night, day, moon
+    transparent = true, -- 是否启用透明背景
+    terminal_colors = true, -- 终端颜色支持
+  },
+}
+
