e1b86908e71357852073176b4ae868b2b3d8b629
wesjos <142285064+nonwesjoe@users.noreply.github.com>
2025-02-11 01:55:03 +0800

    Update zsh.sh

--- zsh/zsh.sh
+++ zsh/zsh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
